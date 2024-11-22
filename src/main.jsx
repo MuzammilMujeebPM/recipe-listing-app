f0edc3b78cc29e34c0d12b275fe91ca852b807c9
MuzammilMujeebPM <muzammilmujeebpm24@gmail.com>
2024-11-22 18:18:09 +0530

    third

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
-import Store from './store/store.js'
+import rStore from './store/store.js'
 import { BrowserRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <Provider store={Store}>
+      <Provider store={rStore}>
         <App />
       </Provider>
     </BrowserRouter>
