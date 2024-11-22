cebf3005fe95a82c6ad7857db0fdf3d79ad13c0a
MuzammilMujeebPM <muzammilmujeebpm24@gmail.com>
2024-11-22 17:58:09 +0530

    Describe the updates you made

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { Provider } from 'react-redux'
-import rStore from './store/store.js'
+import Store from './store/store.js'
 import { BrowserRouter } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
-      <Provider store={rStore}>
+      <Provider store={Store}>
         <App />
       </Provider>
     </BrowserRouter>
